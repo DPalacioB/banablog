4eb88703138bf015cb66c537db9419fb45b7e6f0
dpalaciob <cmparker314@hotmail.com>
2017-01-14 22:05:15 -0500

    Banablog update

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/', function(req, res, next){
+router.post('/test', function(req, res, next){
   console.log(req.body)
   if(req.body.user === 'Bana'){
     res.json({
